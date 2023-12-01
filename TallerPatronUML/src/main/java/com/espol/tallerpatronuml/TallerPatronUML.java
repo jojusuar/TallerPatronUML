962d64c7a0cad4a1a55e84d19ef0838c0723cc7c
jojusuar <jojusuar@espol.edu.ec>
2023-11-30 22:56:23 -0500

    correccion 2

--- TallerPatronUML/src/main/java/com/espol/tallerpatronuml/TallerPatronUML.java
+++ TallerPatronUML/src/main/java/com/espol/tallerpatronuml/TallerPatronUML.java
@@ -13,7 +13,12 @@ public class TallerPatronUML {
 
     public static void main(String[] args) {
         //DECORATOR
-        Notificable notificador = new TelegramNotificador(new WhatsAppNotificador(new WireNotificador(new SignalNotificador(new Notificador("Hola mundo")))));
+        Notificador notificador = new Notificador("Hola mundo");
+        sendEverywhere(notificador);
+    }
+    
+    public static void sendEverywhere(Notificador n){
+        Notificable notificador = new TelegramNotificador(new WhatsAppNotificador(new WireNotificador(new SignalNotificador(n))));
         notificador.notificar();
     }
 }
