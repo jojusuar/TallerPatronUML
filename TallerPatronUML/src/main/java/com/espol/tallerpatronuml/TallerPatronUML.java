8d2ff0608dc8da2edd011866ed6f0c9b582d9a4c
jojusuar <jojusuar@espol.edu.ec>
2023-11-30 20:10:17 -0500

    .

---
+++ TallerPatronUML/src/main/java/com/espol/tallerpatronuml/TallerPatronUML.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.espol.tallerpatronuml;
+
+/**
+ *
+ * @author euclasio
+ */
+public class TallerPatronUML {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
